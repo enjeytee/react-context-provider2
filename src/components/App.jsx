aa563281181d249bf84d10650a8631da7eda3430
enjeytee <enjeytee@gmail.com>
2022-09-02 18:53:30 +0800

    af

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Header from "./Header"
+import Button from "./Button"
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Button />
+        </div>
+    )
+}
+export default App
